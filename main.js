fca9a91e6be10577452893c4f21d3551693e6aba
feliciano0389 <106540070+feliciano0389@users.noreply.github.com>
2023-10-11 09:01:27 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function tocaSomBrasil(){
+    document.querySelector('#som_tecla_brasil').play();
+    }
+document.querySelector('.tecla_brasil').onclick=tocaSomBrasil;
+
+function tocaSomMario(){
+        document.querySelector('#som_tecla_mario').play();
+    }
+document.querySelector('.tecla_mario').onclick=tocaSomMario
+
+function tocaSomPlantao(){
+        document.querySelector('#som_tecla_plantao').play();
+    }
+document.querySelector('.tecla_plantao').onclick=tocaSomPlantao
+
+function tocaSomShow(){
+        document.querySelector('#som_tecla_show').play();
+    }   
+document.querySelector('.tecla_show').onclick=tocaSomShow
